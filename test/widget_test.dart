07abe4282bc77ca8c7423594bb7a7f4c64b2683f
ViLSa <vishnuagbly@gmail.com>
2021-11-14 18:46:49 +0530

    created different file for my app

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:passman/main.dart';
+import 'package:passman/my_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
